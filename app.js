26769276eb759dabbd37719989bc8bf3046fca63
Dinsky21 <dinsky21@gmail.com>
2022-11-01 01:15:52 +0800

    1. revise app.get(/search) function, made multiple search results and category seach results available 2. made code shorter

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const restaurantList = require("./restaurant.json");
+const restaurantList = require("./restaurant.json").results;
 const exphbs = require("express-handlebars");
 
 //射鏡樣本引擎
@@ -16,27 +16,26 @@ app.use(express.static("public"));
 
 // routes setting
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  res.render("index", { restaurants: restaurantList });
 });
 
 //動態路由呈現給予show.handlebars對應的資訊
 app.get("/restaurants/:RestaurantID", (req, res) => {
   let RestaurantID = req.params.RestaurantID;
-  let restaurant = restaurantList.results.find(
-    (r) => r.id === Number(RestaurantID)
-  );
-  res.render("show", { restaurant: restaurant });
+  let restaurant = restaurantList.find((r) => r.id === Number(RestaurantID));
+  res.render("show", { restaurant });
 });
 
 //動態路由，在req.query(<form>才有)中擷取keyword，再搭配filter, includes的功能呈現搜尋結果
 app.get("/search", (req, res) => {
-  let keyword = req.query.keyword.trim();
-  let restaurants = restaurantList.results.filter((r) =>
-    r.name.toLowerCase().includes(keyword.toLowerCase())
+  let keyword = req.query.keyword.trim().toLowerCase();
+  let restaurants = restaurantList.filter(
+    (r) =>
+      r.name.toLowerCase().includes(keyword) || r.category.includes(keyword)
   );
 
-  if (restaurants.length == 1 || keyword == "") {
-    res.render("index", { restaurants: restaurants, keyword: keyword });
+  if (restaurants.length >= 1 || keyword === "") {
+    res.render("index", { restaurants, keyword });
   } else {
     res.render("no_results");
   }
