0ca83959ca4ba9ab13dbbd32f3f32b2c94e66256
Dinsky21 <dinsky21@gmail.com>
2022-12-13 13:30:37 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const restaurantList = require('../../models/restaurant')
 const passport = require('passport')
 const bcrypt = require('bcryptjs')
+
+const restaurantList = require('../../models/restaurant')
+const User = require('../../models/user')
 
 router.get('/login', (req, res) => {
   res.render('login')
@@ -19,6 +21,46 @@ router.get('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email || !password || !confirmPassword) {
+    errors.push({ message: 'the email and password columns are required' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: 'password is not the same' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword,
+    })
+  }
+  User.findOne({ email }).then((user) => {
+    if (user) {
+      errors.push({ message: 'email already exists' })
+      return res.render('register', {
+        errors,
+        name,
+        email,
+        password,
+        confirmPassword,
+      })
+    }
+    return bcrypt
+      .genSalt(10)
+      .then((salt) => bcrypt.hash(password, salt))
+      .then((hash) => {
+        User.create({ name, email, password: hash })
+          .then(res.redirect('/'))
+          .catch((err) => console.log(err))
+      })
+  })
 })
 
 module.exports = router
